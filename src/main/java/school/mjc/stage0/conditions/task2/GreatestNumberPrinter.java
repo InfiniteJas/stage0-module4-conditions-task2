ef9659d9878c6f3de72a5db77acf1e75602f5891
OlzhasAlseitov <olzhas010111@gmail.com>
2023-10-14 22:53:59 +0600

    Module 4.1 task 2

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if (first > second) {
+            return first;
+        } else if (second > first) {
+            return second;
+        } else {
+            return first;
+        }
     }
 }
